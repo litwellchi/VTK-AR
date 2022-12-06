0c045a7af4ab9f2c3273636433b24108112cf307
litwellchi <litwellchi@gmail.com>
2022-12-07 02:29:25 +1200

    start ar project

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+/* eslint-disable no-template-curly-in-string */
+/* eslint-disable no-useless-escape */
+
+module.exports = {
+  module: {
+    rules: [ 
+      { test: /\.css$/, use: 'css-loader' },
+      { test: /\.html$/, use: 'html-loader' },],
+  },
+};
